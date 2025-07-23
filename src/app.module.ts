7799986d07d16a0d9904323c1ef096cf4056b394
SakibJaber <sakibjaber9921@gmail.com>
2025-07-23 12:21:34 +0600

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DomainModule } from 'src/modules/domain.module';
+import { MongooseDatabaseModule } from 'src/common/database/mongoose.module';
+
+@Module({
+  imports: [MongooseDatabaseModule, DomainModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
